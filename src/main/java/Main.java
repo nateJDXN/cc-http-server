478e2dd7eaf3a843330e3264b6ecbd9c12960548
Nate Dixon <retr0@Nates-MacBook-Air.local>
2025-04-21 16:51:02 +0800

    200 response attempt1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,28 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/*
+ * SAMPLE RESPONSE
+
+// Status line
+HTTP/1.1  // HTTP version
+200       // Status code
+OK        // Optional reason phrase
+\r\n      // CRLF that marks the end of the status line
+
+// Headers (empty)
+\r\n      // CRLF that marks the end of the headers
+
+// Response body (empty)
+
+ */
+
 public class Main {
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    
+
     try {
       ServerSocket serverSocket = new ServerSocket(4221);
     
@@ -15,7 +31,8 @@ public class Main {
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
     
-      serverSocket.accept(); // Wait for connection from client.
+      Socket socket = serverSocket.accept(); // Wait for connection from client.
+      socket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
       System.out.println("accepted new connection");
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
