7fd152f7e56568f38d4091b3437e9263abe5a303
Nate Dixon <retr0@Nates-MacBook-Air.local>
2025-04-22 16:52:57 +0800

    verify url

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -26,15 +29,29 @@ public class Main {
 
     try {
       ServerSocket serverSocket = new ServerSocket(4221);
+      System.out.println("Watiting for connection...");
     
       // Since the tester restarts your program quite often, setting SO_REUSEADDR
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
     
       Socket socket = serverSocket.accept(); // Wait for connection from client.
-      socket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+      String request = in.readLine();
+      String[] parts = request.split(" ");
+      String method = parts[0];
+      String path = parts[1];
+      String protocol = parts[2];
+
+      if (path.equals("/")) {
+        out.println(protocol + " 200 OK\r\n\r\n");
+      } else {
+        out.println(protocol + " 404 Not Found\r\n\r\n");
+      }
+      socket.close();
       
-      System.out.println("accepted new connection");
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
